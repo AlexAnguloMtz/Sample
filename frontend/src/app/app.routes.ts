534cd8c10e5cc6796321cc5d7d8397029126b9c0
alex <alexang@agibusiness.com>
2024-09-23 08:40:51 -0700

    Add my page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { IndexComponent } from './index/index.component';
+import { MypageComponent } from './mypage/mypage.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: IndexComponent
+    },
+    {
+        path: 'mypage',
+        component: MypageComponent
     }
 ];
